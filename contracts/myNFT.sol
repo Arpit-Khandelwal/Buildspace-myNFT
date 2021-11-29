6bafd446582ba4bf9226fed78907e02cd74fc289
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-29 13:58:02 +0530

    back to random nft generator

--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -17,26 +17,25 @@ contract myNFT is ERC721URIStorage{
 
     string svg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-    //string[] randomWords = ["This", "is" ,"A", "Random", "Word", "SpongeBob", "Squarepants", "Rebecca", "Netflix" ];
+    string[] randomWords = ["This", "is" ,"A", "Random", "Word", "SpongeBob", "Squarepants", "Rebecca", "Netflix" ];
 
 
     constructor() ERC721("Arpit's First NFT", "ARP"){
         console.log("calling constructor");
     }
 
-    /*function randomWord(uint256 tokenId) public view returns (string memory)
+    function randomWord(uint256 tokenId) public view returns (string memory)
     {
         uint256 rand=uint256(keccak256(abi.encodePacked(string(abi.encodePacked("Random data",Strings.toString(tokenId))))));
 
         rand = rand%randomWords.length;
         return randomWords[rand];
-    }*/
+    }
 
     function makeNFT() public{
         uint256 newItemID = _tokenIds.current();
 
-        //string memory word = randomWord(newItemID);
-        string memory word = "Katik";
+        string memory word = randomWord(newItemID);
         string memory svgFinal = string(abi.encodePacked(svg,word,"</text></svg>"));
 
         string memory json = Base64.encode(bytes(string(abi.encodePacked(
