6ffe988116cb1983a259706c4fc8fe8fcd6d0f24
Arpit Khandelwal <arpit.khandelwal2019@vitstudent.ac.in>
2021-11-29 11:48:08 +0530

    make random svg

--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -12,18 +12,34 @@ contract myNFT is ERC721URIStorage{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+
+    string svg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] randomWords = ["This", "is" ,"A", "Random", "Word", "SpongeBob", "Squarepants", "Rebecca", "Netflix" ];
+
+
     constructor() ERC721("Arpit's First NFT", "ARP"){
-        console.log("my first NFT");
+        console.log("calling constructor\n");
+    }
+
+    function randomWord(uint256 tokenId) public view returns (string memory)
+    {
+        uint256 rand=uint256(keccak256(abi.encodePacked(string(abi.encodePacked("Random data",Strings.toString(tokenId))))));
+
+        rand = rand%randomWords.length;
+        return randomWords[rand];
     }
 
     function makeNFT() public{
         uint256 newItemID = _tokenIds.current();
 
+        string memory svgFinal = string(abi.encodePacked(svg,randomWord(newItemID),"</text></svg>"));
+
         _safeMint(msg.sender, newItemID);
 
-        _setTokenURI(newItemID,"data:application/json;base64,ewoibmFtZSIgOiAiQXJwaXQiLAoiYWdlIiA6ICIxOSIsCiJpbWFnZSIgOiAiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0FnSUR4emRIbHNaVDR1WW1GelpTQjdJR1pwYkd3NklIZG9hWFJsT3lCbWIyNTBMV1poYldsc2VUb2djMlZ5YVdZN0lHWnZiblF0YzJsNlpUb2dNVFJ3ZURzZ2ZUd3ZjM1I1YkdVK0NpQWdJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpQm1hV3hzUFNKaWJHRmpheUlnTHo0S0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krUjJWMElFMWxJR0Z1SUVsdWRHVnlibk5vYVhBOEwzUmxlSFErQ2p3dmMzWm5QZz09Igp9");
+        _setTokenURI(newItemID,"");
 
-        console.log("NFT with ID %s is minted to %s", newItemID,msg.sender);
+        console.log("NFT with ID %s is minted to %s with svg %s", newItemID,msg.sender,svgFinal);
         _tokenIds.increment();
     }
 }
